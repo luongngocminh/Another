c8a87def2c561c3b620858fbc3a5cffab796b1a9
Minh <ngocminhluong182@gmail.com>
2016-11-23 20:50:04 +0700

    Commit

---
+++ Another/Another/MainForm.Designer.cs
@@ -0,0 +1,358 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 05/09/2016
+ * Time: 8:55 CH
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Another
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TabControl tabPages;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.TextBox textEmail;
+		private System.Windows.Forms.TextBox textPass;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.ListBox listChat;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ListBox listName;
+		private System.Windows.Forms.Button btnGetPer;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textEmail = new System.Windows.Forms.TextBox();
+			this.textPass = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tabPages = new System.Windows.Forms.TabControl();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.listName = new System.Windows.Forms.ListBox();
+			this.listChat = new System.Windows.Forms.ListBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.btnGetPer = new System.Windows.Forms.Button();
+			this.tabPage2.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPages.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.WindowText;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.ForeColor = System.Drawing.Color.Lime;
+			this.textBox1.Location = new System.Drawing.Point(12, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(708, 13);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 15);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "User Access Token";
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Black;
+			this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button6.ForeColor = System.Drawing.Color.Lime;
+			this.button6.Location = new System.Drawing.Point(12, 53);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(120, 23);
+			this.button6.TabIndex = 8;
+			this.button6.Text = "Get Access Token";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// textEmail
+			// 
+			this.textEmail.BackColor = System.Drawing.SystemColors.WindowText;
+			this.textEmail.ForeColor = System.Drawing.Color.Lime;
+			this.textEmail.Location = new System.Drawing.Point(564, 104);
+			this.textEmail.Name = "textEmail";
+			this.textEmail.Size = new System.Drawing.Size(155, 20);
+			this.textEmail.TabIndex = 10;
+			this.textEmail.TextChanged += new System.EventHandler(this.TextBox3TextChanged);
+			// 
+			// textPass
+			// 
+			this.textPass.BackColor = System.Drawing.SystemColors.WindowText;
+			this.textPass.ForeColor = System.Drawing.Color.Lime;
+			this.textPass.Location = new System.Drawing.Point(564, 162);
+			this.textPass.Name = "textPass";
+			this.textPass.Size = new System.Drawing.Size(156, 20);
+			this.textPass.TabIndex = 11;
+			this.textPass.TextChanged += new System.EventHandler(this.TextBox4TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(564, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(116, 19);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Phone number or email";
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(564, 145);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 14);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "Password";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.tabPage2.Controls.Add(this.button3);
+			this.tabPage2.Controls.Add(this.textBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(521, 231);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Post ";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(6, 181);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Post to Wall";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.WindowText;
+			this.textBox2.ForeColor = System.Drawing.Color.Lime;
+			this.textBox2.Location = new System.Drawing.Point(6, 6);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(509, 169);
+			this.textBox2.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.tabPage1.Controls.Add(this.checkedListBox1);
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(521, 231);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "User Info";
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.BackColor = System.Drawing.SystemColors.WindowText;
+			this.checkedListBox1.ForeColor = System.Drawing.Color.Lime;
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+			"Id",
+			"Name",
+			"Email",
+			"Birthday"});
+			this.checkedListBox1.Location = new System.Drawing.Point(389, 14);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(117, 139);
+			this.checkedListBox1.TabIndex = 7;
+			this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.CheckedListBox1SelectedIndexChanged);
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.SystemColors.WindowText;
+			this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBox1.ForeColor = System.Drawing.Color.Lime;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(3, 14);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(299, 192);
+			this.listBox1.TabIndex = 3;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1SelectedIndexChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(308, 43);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Clear";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(308, 14);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Get user info";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// tabPages
+			// 
+			this.tabPages.Controls.Add(this.tabPage1);
+			this.tabPages.Controls.Add(this.tabPage2);
+			this.tabPages.Controls.Add(this.tabPage3);
+			this.tabPages.Location = new System.Drawing.Point(12, 82);
+			this.tabPages.Name = "tabPages";
+			this.tabPages.SelectedIndex = 0;
+			this.tabPages.Size = new System.Drawing.Size(529, 257);
+			this.tabPages.TabIndex = 5;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.tabPage3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.tabPage3.Controls.Add(this.listName);
+			this.tabPage3.Controls.Add(this.listChat);
+			this.tabPage3.Controls.Add(this.button4);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(521, 231);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Chat";
+			// 
+			// listName
+			// 
+			this.listName.BackColor = System.Drawing.SystemColors.WindowText;
+			this.listName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listName.ForeColor = System.Drawing.Color.Yellow;
+			this.listName.FormattingEnabled = true;
+			this.listName.ItemHeight = 16;
+			this.listName.Location = new System.Drawing.Point(7, 3);
+			this.listName.Name = "listName";
+			this.listName.Size = new System.Drawing.Size(129, 176);
+			this.listName.TabIndex = 2;
+			this.listName.SelectedIndexChanged += new System.EventHandler(this.ListNameSelectedIndexChanged);
+			this.listName.DoubleClick += new System.EventHandler(this.ListNameDoubleClick);
+			// 
+			// listChat
+			// 
+			this.listChat.BackColor = System.Drawing.SystemColors.WindowText;
+			this.listChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listChat.ForeColor = System.Drawing.Color.Lime;
+			this.listChat.FormattingEnabled = true;
+			this.listChat.ItemHeight = 20;
+			this.listChat.Location = new System.Drawing.Point(142, 3);
+			this.listChat.Name = "listChat";
+			this.listChat.Size = new System.Drawing.Size(373, 180);
+			this.listChat.TabIndex = 1;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(7, 195);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "Get Chat";
+			this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// btnGetPer
+			// 
+			this.btnGetPer.BackColor = System.Drawing.SystemColors.ControlText;
+			this.btnGetPer.ForeColor = System.Drawing.Color.Lime;
+			this.btnGetPer.Location = new System.Drawing.Point(139, 53);
+			this.btnGetPer.Name = "btnGetPer";
+			this.btnGetPer.Size = new System.Drawing.Size(75, 23);
+			this.btnGetPer.TabIndex = 14;
+			this.btnGetPer.Text = "Get Permit";
+			this.btnGetPer.UseVisualStyleBackColor = false;
+			this.btnGetPer.Click += new System.EventHandler(this.BtnGetPerClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.ClientSize = new System.Drawing.Size(731, 357);
+			this.Controls.Add(this.btnGetPer);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textPass);
+			this.Controls.Add(this.textEmail);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tabPages);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Another";
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage1.ResumeLayout(false);
+			this.tabPages.ResumeLayout(false);
+			this.tabPage3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Another/Another/MainForm.cs
@@ -0,0 +1,329 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 05/09/2016
+ * Time: 8:55 CH
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+
+using System.Windows.Forms;
+using Facebook;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Chrome;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace Another
+{
+
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public string json {get; set;}
+
+		public static string urlPermits = "https://m.facebook.com/v1.0/dialog/oauth?redirect_uri=https://account.nokia.com/Copy_this_whole_url_www.facebook.com&scope=email,publish_actions,user_about_me,user_actions.books,user_actions.music,user_actions.news,user_actions.video,user_activities,user_birthday,user_education_history,user_events,user_games_activity,user_groups,user_hometown,user_interests,user_likes,user_location,user_notes,user_photos,user_questions,user_relationship_details,user_relationships,user_religion_politics,user_status,user_subscriptions,user_videos,user_website,user_work_history,friends_about_me,friends_actions.books,friends_actions.music,friends_actions.news,friends_actions.video,friends_activities,friends_birthday,friends_education_history,friends_events,friends_games_activity,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_photos,friends_questions,friends_relationship_details,friends_relationships,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history,ads_management,create_event,create_note,export_stream,friends_online_presence,manage_friendlists,manage_notifications,manage_pages,photo_upload,publish_stream,read_friendlists,read_insights,read_mailbox,read_page_mailboxes,read_requests,read_stream,rsvp_event,share_item,sms,status_update,user_online_presence,video_upload,xmpp_login&response_type=token%20code&client_id=200758583311692" ;
+		public static string urlAccessToken = "https://developers.facebook.com/tools/debug/accesstoken/?app_id=200758583311692";
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			/*string[] theCookies = System.IO.Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Cookies));
+foreach (string currentFile in theCookies)
+{
+   try
+   {
+      System.IO.File.Delete(currentFile);
+   }
+   catch (Exception ex)
+   {
+   }
+}*/
+		
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			FacebookClient client = new FacebookClient(textBox1.Text);
+			dynamic me = client.Get("me");
+			string id = me.id;
+			string name = me.name;
+			string email = me.email;
+			string birthday = me.birthday;
+			
+			for(int i = 0; i < checkedListBox1.CheckedItems.Count; i++){
+				switch ((string)checkedListBox1.CheckedItems[i]) {
+					case "Email":
+						listBox1.Items.Add("Email:"+ email);
+						break;
+					case "Birthday":
+						listBox1.Items.Add("Birthday:"+birthday);
+						break;
+					case "Id":
+						listBox1.Items.Add("ID:"+id);
+						break;
+					case "Name":
+						listBox1.Items.Add("Name:"+name);
+						break;
+					default:
+						
+						break;
+				}
+			}
+		}
+		void ListBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+		}
+		void CheckedListBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			var client = new FacebookClient(textBox1.Text);
+			
+			dynamic result = client.Post("/me/feed", new { message = textBox2.Text });
+			
+		}
+		
+		void WebBrowser1DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			
+		}
+		private bool IsElementPresent(By by)
+    {
+        try
+        {
+        	IWebDriver driver = new PhantomJSDriver();
+            driver.FindElement(by);
+            return true;
+        }
+        catch (NoSuchElementException)
+        {
+        	MessageBox.Show("ERROR !");
+        	
+        	
+            return false;
+            
+        }
+    }
+		
+		void Button5Click(object sender, EventArgs e)
+		{
+			
+			
+		
+	
+		}
+		void ListBox2SelectedIndexChanged(object sender, EventArgs e)
+		{
+	
+		}
+		async void Button6Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(textEmail.Text) || string.IsNullOrWhiteSpace(textPass.Text)){
+				MessageBox.Show("Please fill out the Login form!");
+			} else {
+			IWebDriver driver = new PhantomJSDriver();
+			
+			driver.Navigate().GoToUrl("https://www.facebook.com");
+			LoginFacebook(driver);
+			driver.Navigate().GoToUrl(urlPermits);
+			await PutTaskDelay();
+			/*if(driver.FindElements(By.CssSelector("[action='/v2.0/dialog/oauth/confirm']")).Count != 0 ) {
+			driver.FindElement(By.Name("__CONFIRM__")).Click();
+			} else {
+			driver.FindElement(By.Name("__CONFIRM__")).Click();
+			await PutTaskDelay();
+			driver.FindElement(By.Name("__CONFIRM__")).Click();
+			await PutTaskDelay();
+			driver.FindElement(By.Name("__CONFIRM__")).Click();
+			await PutTaskDelay();
+			}
+			driver.Navigate().GoToUrl(urlAccessToken);
+			
+			
+			IWebElement query = driver.FindElement(By.Name("q"));
+			string AccessToken = query.GetAttribute("value");
+			*/
+			string accessUrl = driver.Url;
+			string AccessToken = accessUrl.Split(new char[] { '=' }, 2).Last();
+			textBox1.Text = AccessToken;
+			driver.Quit();
+			
+			
+			
+			
+			
+			}
+		}
+		void Button7Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void ListBox4SelectedIndexChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void Label2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox3TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox4TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		private void LoginFacebook (IWebDriver driver)
+		{
+			
+			
+			IWebElement email = driver.FindElement(By.Name("email"));
+			email.SendKeys(textEmail.Text);
+			IWebElement pass = driver.FindElement(By.Name("pass"));
+			pass.SendKeys(textPass.Text);
+			IWebElement loginBtn = driver.FindElement(By.Id("loginbutton"));
+			loginBtn.Click();
+		}
+		
+		async Task PutTaskDelay()
+		{
+    		await Task.Delay(4000);
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			listName.Items.Clear();
+			using (WebClient wc = new WebClient())
+		{
+			wc.Headers.Add("Accept-Language", " en-US");
+       		wc.Headers.Add("Accept", " text/html, application/xhtml+xml, */*");
+        	wc.Headers.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
+  			 string s = wc.DownloadString("https://graph.facebook.com/me/inbox?access_token=" + textBox1.Text);
+  			 json = s;
+  			 dynamic messages = JsonConvert.DeserializeObject(json);
+  			 
+  			
+  			 var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+			Dictionary<string,object> csObj = serializer.Deserialize<Dictionary<string,object>>(json);
+			int length = ((ArrayList)csObj["data"]).Count; 
+  			 
+  			 
+  			 for (int i = 0; i < length; i ++ ){
+  			
+  			 string messager = messages.data[0].comments.data[i].from.name.Value;
+  			 JObject comment =  messages.data[0].comments.data[i];
+  			 JArray friendName = messages.data[i].to.data;
+  			 string id = messages.data[i].id.Value;
+  			 if (friendName.Count > 2){
+  			 	listName.Items.Add("A Group" + ":" + id);
+  			 } else {
+  			 	string friendsName = messages.data[i].to.data[0].name.Value;
+  			 	
+  			 	listName.Items.Add(friendsName + ":"  +id);
+  			 }
+  			 
+  			 
+  			 
+  			 
+  			 
+		
+		}
+  			 }
+			}
+		void ListNameSelectedIndexChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void ListNameClick(object sender, EventArgs e)
+		{
+			
+		}
+		void ListNameDoubleClick(object sender, EventArgs e)
+		{
+			listChat.Items.Clear();
+				
+  			 dynamic messages = JsonConvert.DeserializeObject(json);
+  			 
+  			 
+  			 try{
+			if (listName.SelectedItem != null)
+			{
+				string sub = listName.SelectedItem.ToString().Split(':').Last();
+				JToken id = messages.SelectToken("$.data[?(@.id == '"+ sub +"')]");
+				
+				dynamic curMess = JsonConvert.DeserializeObject(id.ToString());
+				JObject mymess = curMess.comments;
+				string mess = mymess.ToString();
+				var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+				Dictionary<string,object> csObj = serializer.Deserialize<Dictionary<string,object>>(mess);
+				int length = ((ArrayList)csObj["data"]).Count; 
+				for(int i = 0; i < length; i ++){
+					JObject comments = curMess.comments.data[i];
+					string messSender = curMess.comments.data[i].from.name.Value;
+					if (comments["message"] != null)
+  			 		{
+					
+					string messS = curMess.comments.data[i].message.Value;
+					
+					listChat.Items.Add(messSender + ": " + messS);
+					} 
+					else {
+					listChat.Items.Add(messSender + ": <An Icon>" );
+					}
+				}
+			}
+  			 } catch (Exception ex){}
+		} 
+		
+		void BtnGetPerClick(object sender, EventArgs e)
+		{
+			if(string.IsNullOrWhiteSpace(textEmail.Text) || string.IsNullOrWhiteSpace(textPass.Text)){
+				MessageBox.Show("Please fill out the Login form!");
+			} else {
+			IWebDriver driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(urlPermits);
+			LoginFacebook(driver);
+			DialogResult d = MessageBox.Show("When you done permiting, click 'OK' button", "Alert", MessageBoxButtons.OK);
+			if(d == DialogResult.OK)
+			{
+				driver.Quit();
+			}
+			
+			}
+				
+		}
+	
+		}		
+    }
+
+
